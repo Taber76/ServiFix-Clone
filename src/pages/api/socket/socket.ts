68b24656ac7a765d4808757d9523e29f74c57907
Tabare Bermudez <tabarebermudez@gmail.com>
2024-06-03 09:39:47 -0300

    feat(Backend): :bug: Get connected users to socket

--- src/pages/api/socket/socket.ts
+++ src/pages/api/socket/socket.ts
@@ -2,35 +2,39 @@ import type { NextApiRequest } from "next";
 import { NextApiResponseServerIO } from "@/types/next.types";
 import { Server as ServerIO } from "socket.io";
 import { Server as NetServer } from "http";
-import AuthHelper from "@/server/helpers/auth.helper";
+import { prisma } from "@/server/lib/prisma";
+import ChatController from "@/server/controllers/chat.controller";
+import AuthMiddleware from "@/server/middlewares/auth.middleware";
 
 export default function handler(req: NextApiRequest, res: NextApiResponseServerIO) {
+
+  const middlewareResponse = AuthMiddleware.checkAuth(req, ['ADMIN', 'CUSTUMER', 'SUPPLIER']);
+  if (!middlewareResponse.success) return res.status(401).json({ msg: 'Unauthorized.' });
+  const { user_id } = middlewareResponse
 
   if (!res.socket.server.io) {
 
     const httpServer: NetServer = res.socket.server as any
     const io: ServerIO = new ServerIO(httpServer)
 
-    io.on('connection', (socket) => {
-      let authenticated = false;
+    io.on('connection', async (socket) => {
+      let online = await prisma.online.findFirst({ where: { user_id: Number(user_id) } })
+      if (online) {
+        online = await prisma.online.update({ where: { id: online.id }, data: { socket_id: socket.id } })
+      } else {
+        online = await prisma.online.create({ data: { user_id: Number(user_id), socket_id: socket.id } })
+      }
 
-      socket.on('authenticate', async (token) => {
-        if (AuthHelper.authenticateUserToken(token, ['ADMIN', 'CUSTUMER', 'SUPPLIER']).success) {
-          authenticated = true;
-          socket.emit('authenticated', socket.id);
+      socket.on('message', async (msg, recipientId) => {
+        const recipient = await prisma.online.findFirst({ where: { user_id: recipientId } })
+        if (recipient) {
+          io.to(recipient.socket_id).emit('message', msg, socket.id);
         }
-      })
-
-      socket.on('message', async (msg, socketRecipientId) => {
-        if (!authenticated) {
-          socket.emit('unauthenticated');
-          return
-        };
-        io.to(socketRecipientId).emit('message', msg, socket.id);
+        await ChatController.socketSaveMessage(Number(user_id), Number(recipientId), msg)
       })
 
       socket.on('disconnect', () => {
-        authenticated = false;
+        prisma.online.delete({ where: { id: online.id } })
       });
     });
 
