a3af42d57bf2739462cb27ca15794fc2f3265b09
Tabare Bermudez <tabarebermudez@gmail.com>
2024-06-07 12:01:52 -0300

    feat(Frontend): :sparkles: Add chat component

--- src/components/ChatDialog.tsx
+++ src/components/ChatDialog.tsx
@@ -1,10 +1,6 @@
 import {
     Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
+    DialogContent
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import io, { Socket } from 'socket.io-client';
@@ -18,8 +14,11 @@ export function ChatDialogModal({ isOpen, setIsOpen, recipientId }:
 
     const [message, setMessage] = useState('')
     const [messages, setMessages] = useState<any[]>([]);
+    const [isConnected, setIsConnected] = useState(false);
     const socketRef = useRef<Socket | null>(null);
     const messagesEndRef = useRef<HTMLDivElement | null>(null);
+
+    const contactImage = "https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144"
 
     useEffect(() => {
         const fetchChatHistory = async () => {
@@ -65,40 +64,70 @@ export function ChatDialogModal({ isOpen, setIsOpen, recipientId }:
 
     return (
         <Dialog open={isOpen} onOpenChange={setIsOpen}>
-            <DialogContent className="sm:max-w-[425px] bg-blue-800/50 border border-green-600">
+            <DialogContent className="md:max-w-[600px] max-w-[400px]bg-blue-800/50 max-h-[90%] border border-green-600 p-2">
 
-                <DialogHeader>
-                    <DialogTitle>Chat</DialogTitle>
-                    <DialogDescription>
-                        Chat con el usuario.
-                    </DialogDescription>
-                </DialogHeader>
+                <div className="flex-1 flex flex-col h-full">
 
-                <div className="grid gap-1 py-1">
-                    <div className="max-h-80 overflow-y-auto p-2 border border-green-600 rounded bg-white">
+                    {/*Avatar and username*/}
+                    <div className="flex sm:items-center justify-between py-1 border-b-2 border-gray-200">
+                        <div className="relative flex items-center space-x-4">
+                            <div className="relative">
+                                {isConnected &&
+                                    <span className="absolute text-green-500 right-0 bottom-0">
+                                        <svg width="20" height="20">
+                                            <circle cx="8" cy="8" r="8" fill="currentColor"></circle>
+                                        </svg>
+                                    </span>
+                                }
+                                <img src={contactImage} alt="" className="w-5 sm:w-12 h-5 sm:h-12 rounded-full" />
+                            </div>
+                            <div className="flex flex-col leading-tight">
+                                <div className="text-2xl mt-1 flex items-center">
+                                    <span className="text-gray-700 mr-3">Nombre contacto</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Messages*/}
+                    <div id="messages" className="h-[60vh] flex flex-col space-y-4 p-3 overflow-y-auto"
+                        style={{ scrollbarWidth: 'thin', scrollbarColor: '#edf2f7 #f7fafc' }}
+                        ref={messagesEndRef}
+                    >
+
                         {messages.map((msg, index) => (
-                            <div key={index} className={`p-2 mb-1 rounded ${msg.sender_id !== recipientId ? 'bg-green-200 text-right ml-5' : 'bg-gray-200 self-end text-left mr-5'}`}>
-                                {msg.message}
+                            <div key={index} className={`flex ${msg.sender_id == recipientId ? 'justify-start' : 'justify-end'}`}>
+                                <div className="flex items-end">
+                                    <div className={`flex flex-col space-y-2 text-xs max-w-xs mx-2 ${msg.sender_id == recipientId ? 'items-start order-2' : 'items-end order-1'}`}>
+                                        <div><span className={`px-4 py-2 rounded-lg inline-block rounded-bl-none ${msg.sender_id == recipientId ? 'bg-gray-300 text-gray-600' : 'bg-blue-600 text-blue-100'}`}>{msg.message}</span></div>
+                                    </div>
+                                    {msg.sender_id == recipientId && <img src={contactImage} alt="My profile" className="w-6 h-6 rounded-full order-1" />}
+                                </div>
                             </div>
                         ))}
-                        <div ref={messagesEndRef} />
-                    </div>
-                    <textarea
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        className="p-2 border border-gray-300 rounded resize-none min-h-[50px] max-h-[150px] w-full overflow-y-auto"
-                        placeholder="Type your message..."
-                    />
-                </div>
 
-                <DialogFooter className="flex flex-row justify-evenly items-center gap-1">
-                    <Button
-                        variant="default"
-                        color="access"
-                        onClick={handleSendMessage}>
-                        Send
-                    </Button>
-                </DialogFooter>
+
+                    </div>
+
+                    {/* Typing area */}
+                    <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0" >
+                        <div className="relative flex">
+                            <input type="text" placeholder="Write your message!" className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3"
+                                value={message} onChange={(e) => { setMessage(e.target.value) }} />
+                            <div className="absolute right-0 items-center inset-y-0 hidden sm:flex">
+                                <button type="button" className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none"
+                                    onClick={() => { handleSendMessage() }}
+                                >
+                                    <span className="font-bold">Send</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 ml-2 transform rotate-90">
+                                        <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
 
             </DialogContent>
         </Dialog>
